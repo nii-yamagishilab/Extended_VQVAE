8c8a1de851aad82e7ac7cebb96fb6abb2ecb9bdb
YiAthena <zhaoyi@nii.ac.jp>
2020-05-21 22:06:55 +0900

    add copyright head

---
+++ VC/VQVAE_F0/inference.py
@@ -0,0 +1,71 @@
+# ==============================================================================
+# Copyright (c) 2020, Yamagishi Laboratory, National Institute of Informatics
+# Author: Yi Zhao (zhaoyi@nii.ac.jp)
+# All rights reserved.
+# ==============================================================================
+import math, pickle, os
+import numpy as np
+import torch
+from torch.autograd import Variable
+from torch import optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from utils import *
+import sys
+import models.nocond as nc
+import models.vqvae as vqvae
+import models.wavernn1 as wr
+import utils.env as env
+import argparse
+import platform
+import re
+import utils.logger as logger
+import time
+import subprocess
+
+import models.wavernn1 as wr
+import config
+import librosa
+
+
+if __name__ == "__main__":
+    test_files_txt = config.test_files_txt
+
+    output_dir = config.test_output_dir
+    os.makedirs(output_dir, exist_ok=True)
+
+    model_path = config.test_model_path
+    sample_rate = config.sample_rate
+    batch_size = config.batch_size
+    
+
+    model = wr.Model(rnn_dims=config.rnn_dims, fc_dims=config.fc_dims, pad=2,
+                  upsample_factors=config.upsample_factors, feat_dims=80).cuda()
+    model.load_state_dict(torch.load(model_path))
+
+    with open(test_files_txt) as f:
+        test_files = f.readlines()
+    # print(test_files)
+    test_files = [x.strip() for x in test_files]
+    total_num_files = len(test_files)
+    print('number of files: %d' %(total_num_files))
+    for i in range(int(total_num_files/batch_size) + 1):
+        if (i+1) * batch_size <= total_num_files:
+            files = test_files[(i*batch_size) : ((i+1) * batch_size)]
+        else:
+            files = test_files[(i*batch_size) : total_num_files]
+        if len(files) == 0:
+            print('no more files')
+            exit ()
+        test_mels = [np.load(x) for x in files]
+        maxlen = max([x.shape[1] for x in test_mels])
+        len_test_mels = [x.shape[1] for x in test_mels]
+        aligned = [torch.cat([torch.FloatTensor(x), torch.zeros(80, maxlen-x.shape[1]+1)], dim=1) for x in test_mels]
+        #print(torch.stack(aligned).size())
+        out = model.forward_generate((torch.stack(aligned)).cuda(), deterministic=True)
+        for j in range(len(files)):
+            audio = out[j][:((len_test_mels[j]-3)*config.hop_length)].cpu().numpy()
+            #print(audio.shape)
+            basename = os.path.basename(files[j]).split('.')[0]
+            librosa.output.write_wav(os.path.join(output_dir, basename+'.wav'), audio, sr = sample_rate)
---
+++ VC/VQVAE_F0/preprocess_f0.py
@@ -0,0 +1,136 @@
+# ==============================================================================
+# Copyright (c) 2020, Yamagishi Laboratory, National Institute of Informatics
+# Author: Yi Zhao (zhaoyi@nii.ac.jp)
+# All rights reserved.
+# ==============================================================================
+import sys
+import glob
+import pickle
+import os
+import multiprocessing as mp
+from utils.dsp import *
+import pandas as pd
+import multiprocessing as mp
+import config
+
+def read_binfile(filename, dim=60, dtype=np.float64):
+    '''
+    Reads binary file into numpy array.
+    '''
+    fid = open(filename, 'rb')
+    v_data = np.fromfile(fid, dtype=dtype)
+    fid.close()
+    if np.mod(v_data.size, dim) != 0:
+        raise ValueError('Dimension provided not compatible with file size.')
+    m_data = v_data.reshape((-1, dim)).astype('float64') # This is to keep compatibility with numpy default dtype.
+    m_data = np.squeeze(m_data)
+    return  m_data
+
+def process_file(wav_file, f0_file, out_path):
+    name = wav_file.split('/')[-1][:-4] # Drop .wav
+
+    filename = f'{out_path}/quant/{name}.npy'
+    # if os.path.exists(filename):
+    #     print(f'{filename} already exists, skipping')
+    #     return
+
+    floats = load_wav(wav_file, encode=False)
+
+    # trimmed, _ = librosa.effects.trim(floats, top_db=25) ##trimmed before
+    quant = (floats * (2**15 - 0.5) - 0.5).astype(np.int16)
+    if max(abs(quant)) < 2048:
+        print(f'audio fragment too quiet ({max(abs(quant))}), skipping: {wav_file}')
+        return
+    if len(quant) < 5000:
+        print(f'audio fragment too short ({len(quant)} samples), skipping: {wav_file}')
+        return
+    os.makedirs(out_path, exist_ok=True)
+
+
+    f0 = read_binfile(f0_file, dim=1, dtype=np.float32)
+    filename_f0 = f'{out_path}/f0/{name}.npy'
+
+
+    audio_ref_size = len(f0)*config.hop_length
+
+    frm_diff = len(quant) - audio_ref_size
+    print (len(quant), audio_ref_size, frm_diff)
+    if frm_diff <0:
+        quant = np.r_[ quant, np.zeros(-frm_diff) + quant[-1]]
+    if frm_diff > 0:
+        quant = quant[:-frm_diff]
+
+    if  abs(len(quant)-len(f0)*config.hop_length) > 2 :
+        print(len(quant), len(f0), len(quant)-len(f0)*config.hop_length, filename)
+
+    np.save(filename, quant)
+    np.save(filename_f0, f0)
+    return name
+
+
+def main():
+    data_path = sys.argv[1]
+    wav_path = os.path.join(data_path, 'wavs', 'wav_22050_trimmed_normalized')
+    if0_path = os.path.join(data_path, 'features', 'F0_confidence/F0')
+    scp_path = os.path.join(data_path, 'scp')
+    all_scp = os.path.join(data_path, 'all.scp')
+    gender_emo_scp = os.path.join(scp_path, 'utt_gender_emotion.csv')
+    spk_emd_scp = os.path.join(scp_path, 'spk_ebd.csv')
+    out_path = os.path.join(data_path, 'preprocessed_data_f0c')
+
+    df_gender_emo = pd.read_csv(gender_emo_scp, sep="\t", dtype=object, header=0)
+    df_spk_emd = pd.read_csv(spk_emd_scp, sep="\t", dtype=object, header=0)
+
+
+    files = [os.path.join(wav_path, x)+'.wav' for x in df_gender_emo['utt'].values]
+
+    os.makedirs(os.path.join(out_path, 'quant'), exist_ok=True)
+    os.makedirs(os.path.join(out_path, 'f0'), exist_ok=True)
+    index = []
+    with mp.Pool(2*mp.cpu_count()+1) as pool:
+        res = pool.starmap_async(process_file, [(f, os.path.join(if0_path, os.path.basename(f).split('.')[0]+'.f0'), out_path) for f in files]).get()
+
+        index.extend([x for x in res if x])
+
+
+    # index = df_gender_emo['utt'].values
+    with open(f'{out_path}/index.pkl', 'wb') as f:
+        pickle.dump(index, f)
+
+
+def main_ch():
+    data_path = sys.argv[1]
+    wav_path = os.path.join(data_path, 'wavs', 'wav_22050_trimmed_normalized')
+    if0_path = os.path.join(data_path, 'features', 'f0_22050_64_crepe')
+    scp_path = os.path.join(data_path, 'scp')
+    scp_f0 = os.path.join(scp_path, 'scp_f0')
+    # gender_emo_scp = os.path.join(scp_path, 'utt_gender_emotion.csv')
+    # spk_emd_scp = os.path.join(scp_path, 'spk_ebd.csv')
+    out_path = os.path.join(data_path, 'preprocessed_data_f0c')
+
+    # df_gender_emo = pd.read_csv(gender_emo_scp, sep="\t", dtype=object, header=0)
+    # df_spk_emd = pd.read_csv(spk_emd_scp, sep="\t", dtype=object, header=0)
+    #
+    #
+    # files = [os.path.join(wav_path, x)+'.wav' for x in df_gender_emo['utt'].values]
+    with open(scp_f0, 'r') as f:
+        files = f.readlines()
+    files = [x.strip() for x in files]
+
+    os.makedirs(os.path.join(out_path, 'quant'), exist_ok=True)
+    os.makedirs(os.path.join(out_path, 'f0'), exist_ok=True)
+    index = []
+    with mp.Pool(2*mp.cpu_count()+1) as pool:
+        res = pool.starmap_async(process_file, [(os.path.join(wav_path, f) + '.wav', os.path.join(if0_path, os.path.basename(f).split('.')[0]+'.f0'), out_path) for f in files]).get()
+
+        index.extend([x for x in res if x])
+
+
+    # index = df_gender_emo['utt'].values
+    with open(f'{out_path}/index.pkl', 'wb') as f:
+        pickle.dump(index, f)
+
+
+if __name__ == '__main__':
+    # main()
+    main_ch()
