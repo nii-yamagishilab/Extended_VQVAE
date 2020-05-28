a433b73da6e8d09884c71216f73884942cefc76c
YiAthena <zhaoyi@nii.ac.jp>
2020-05-28 15:12:51 +0900

    change head

--- utils/dsp.py
+++ utils/dsp.py
@@ -1,6 +1,7 @@
 # ==============================================================================
 # Copyright (c) 2020, Yamagishi Laboratory, National Institute of Informatics
-# Author: Yi Zhao (zhaoyi@nii.ac.jp)
+# Original: https://github.com/mkotha/WaveRNN
+# Modified: Yi Zhao (zhaoyi[at]nii.ac.jp)
 # All rights reserved.
 # ==============================================================================
 import numpy as np
